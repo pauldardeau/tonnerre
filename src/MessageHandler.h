18f7c09b75d9c542596626b0ae7472d021a3b371
pauldardeau <pauldardeau@me.com>
2014-07-25 21:16:16 -0500

    added javadoc comments

--- src/MessageHandler.h
+++ src/MessageHandler.h
@@ -10,17 +10,40 @@ class KeyValuePairs;
 class Message;
 
 
+/**
+ * MessageHandler is the interface (abstract based class) to use for all
+ * custom message handlers.
+ */
 class MessageHandler
 {
 public:
    virtual ~MessageHandler() {}
    
+   /**
+    * Handles a message with a text payload (Text type)
+    * @param requestMessage the request message
+    * @param responseMessage the response message
+    * @param requestName the name of the request
+    * @param requestPayload the request payload text
+    * @param responsePayload the response payload text
+    * @see Message()
+    */
    virtual void handleTextMessage(std::shared_ptr<Message> requestMessage,
                                   Message& responseMessage,
                                   const std::string& requestName,
                                   const std::string& requestPayload,
                                   std::string& responsePayload) = 0;
-   
+
+   /**
+    * Handles a message with a payload of key-value pairs (KeyValues type)
+    * @param requestMessage the request message
+    * @param responseMessage the response message
+    * @param requestName the name of the request
+    * @param requestPayload the request payload as key-value pairs
+    * @param responsePayload the response payload as key-value pairs
+    * @see Message()
+    * @see KeyValuePairs()
+    */
    virtual void handleKeyValuesMessage(std::shared_ptr<Message> requestMessage,
                                        Message& responseMessage,
                                        const std::string& requestName,
